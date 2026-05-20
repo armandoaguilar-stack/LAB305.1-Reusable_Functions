49ff6b1db63695e90d81def7da04da31727fdbca
armandoaguilar-stack <armandinhobarca10@gmail.com>
2026-05-19 21:57:04 -0400

    Finding #s to End and Printing Them

--- LAB308.5.1_Reustable_Functions.js
+++ LAB308.5.1_Reustable_Functions.js
@@ -53,3 +53,30 @@ function stringsLongerThan(arr, length){
 console.log(`\nTake an array of strings, #, and return strings longer than the given number`)
 console.log(`Strings longer than 7 characters, our new array is: `)
 console.log(stringsLongerThan(Array2, 7)); // printing results of functions 
+
+// Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+function stringsLongerThan(arr, length){
+  //return a filted array       //condtion to be included in the filtered array
+  return arr.filter( (element)=> element.length>length );
+}
+console.log(`\nTake an array of strings, and a number and return an array of the strings that are longer than the given number`)
+console.log(`When we want only strings longer than 7 characters, our new array is: `)
+console.log(stringsLongerThan(Array2, 7));
+
+
+
+console.log(`\nTake a number, n, and print every number between 1 and n` )
+function printNums(endNumber, currentNumber=1){ //default second paramter set to 1, bc we want to start counting up from 1
+
+    //base case when we reach end num, just print it
+    if(currentNumber===endNumber){
+      console.log(endNumber);
+      return
+    }
+    //recursive call, print out current number then call it again with currentnumer +1 to replace the default of 1 to do the counting up
+    console.log(currentNumber)
+    return printNums (endNumber,currentNumber+1);
+       
+}
+printNums(20); // Prints all #s between 1 and 20, inclusive. 
+console.log() // Takes a #, n, and prints every # btwn 1 and n(20) in order.
